cfa826a7d4f947e45743d63b134f442d38165988
Mazzi <75170536+davideMazzitelli@users.noreply.github.com>
2022-10-25 14:24:06 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,15 +5,17 @@ class VolumePanel(bpy.types.Panel):
     bl_idname = "OBJECT_PT_VolumePanel"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = "Volume"
+    bl_category = "MeshVolume"
 
     def draw(self, context):
         layout = self.layout
+        scene = context.scene
         
         row = layout.row()
         row.label(text = "calculate  volume", icon ="META_CUBE")
 
-        row = layout.row(align=True)
+        col = layout.column(align=True)
+        row = col.row(align=True)
         row.operator("object.mesh_volume")
         row.operator("object.save_info")
 
@@ -23,4 +25,28 @@ class VolumePanel(bpy.types.Panel):
         if float(bpy.context.object.volume) > 0:
             row = layout.row()
             row.alignment = 'CENTER'
-            row.label(text = "%.4f" % bpy.context.object.volume +" m\u00b3")
+            row.label(text = "%.6f" % bpy.context.object.volume +" m\u00b3")
+        
+        row = layout.row()
+        row.operator("object.union_operator")
+        row = layout.row()
+        row.operator("db.export_operator")
+
+class BackgroundPanel(bpy.types.Panel):
+    bl_label = "background"
+    bl_idname = "_PT_BackgroundPanel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "MeshVolume"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        menu = scene.background
+
+        row = layout.row()
+        layout.prop(menu, "backgroundMenu")
+        row = layout.row()
+        row.operator("background.choose_background")
+
